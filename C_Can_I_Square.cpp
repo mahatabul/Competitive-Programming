67d27ca4ea302959051d2bf12f9eada56ab34451
Mahatabul Islam <149324015+mahatabul@users.noreply.github.com>
2024-06-15 00:08:47 +0600

    Add files via upload

--- C_Can_I_Square.cpp
+++ C_Can_I_Square.cpp
@@ -5,29 +5,31 @@ using ll = long long;
 
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-bool isWholeNumber(double number) {
-    return fmod(number, 1.0) == 0.0;
-}
 
 int main() {
     fastio;
 
-    int t;
+    ll t;
     cin>>t;
     while (t--)
     {
-        int n;
+        ll n;
         cin>>n;
-        int num=0;
-        for (int i = 0; i < n; i++)
+        ll num=0;
+        for (auto i = 0; i < n; i++)
         {
-            int a;cin>>a;
+            ll a;cin>>a;
             num+=a;
         }
         
-        double ans = sqrt(num);
-        (isWholeNumber(ans)? cout << "YES":cout<<"NO");
-        cout<<endl;
+        ll ans = sqrt(num);
+        if (ans*ans==num)
+        {
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+        
+        
     }
     
 
